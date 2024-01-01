5f89b6509686131ceca919a045e2e223287b033f
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2024-01-02 02:09:27 +0700

    fix: add next/image props

--- apps/web/landing/src/components/product/Product.tsx
+++ apps/web/landing/src/components/product/Product.tsx
@@ -4,7 +4,7 @@ export default function Product() {
   return (
     <a
       href="https://monomod.studio/"
-      className="ring-1 lg:row-span-2 flex flex-col justify-between group hover:ring-primary/10 dark:hover:ring-white/20 duration-300 h-full dark:ring-white/10 ring-primary/5 lg:row-start-2 md:grid-cols-2 md:grid lg:gap-0 md:items-center md:gap-12 lg:grid-cols-none lg:col-start-2 lg:col-span-2 rounded-3xl p-8 bg-white dark:bg-secondary shadow-xl dark:shadow-thick"
+      className="relative ring-1 lg:row-span-2 flex flex-col justify-between group hover:ring-primary/10 dark:hover:ring-white/20 duration-300 h-full dark:ring-white/10 ring-primary/5 lg:row-start-2 md:grid-cols-2 md:grid lg:gap-0 md:items-center md:gap-12 lg:grid-cols-none lg:col-start-2 lg:col-span-2 rounded-3xl p-8 bg-white dark:bg-secondary shadow-xl dark:shadow-thick"
     >
       <div>
         <p className="ext-xl tracking-tight font-medium text-primary dark:text-white md:text-6xl">
@@ -21,8 +21,11 @@ export default function Product() {
       </div>
       <div className="mt-8">
         <Image
+          width={800}
+          height={200}
           src="/images/monomod.png"
           className="rounded-2xl group-hover:ring-white/20 duration-300 invert dark:invert-0 aspect-[4/4] bg-primary/5 dark:bg-primary ring-1 ring-white/10  object-cover"
+          objectFit="cover"
           alt=""
         />
       </div>
