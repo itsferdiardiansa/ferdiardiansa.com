409712f4c28b4f13d48383be9d2308a6e16ef29a
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2024-01-02 22:27:55 +0700

    refactor: delete unused components

--- apps/web/landing/src/app/(landing)/page.tsx
+++ apps/web/landing/src/app/(landing)/page.tsx
@@ -1,12 +1,7 @@
 import AboutMe from '@/components/about-me/AboutMe'
-import Activity from '@/components/activity/Activity'
-import Community from '@/components/community/Community'
 import Description from '@/components/description/Description'
 import FindMe from '@/components/find-me/FindMe'
-import GrabResume from '@/components/grab-resume/GrabResume'
-import Newsletter from '@/components/newsletter/Newsletter'
 import Product from '@/components/product/Product'
-import Project from '@/components/project/Project'
 
 export default function Home() {
   return (
@@ -15,19 +10,9 @@ export default function Home() {
 
       <FindMe />
 
-      {/* <GrabResume /> */}
-
-      {/* <Project /> */}
-
       <Description />
 
       <Product />
-
-      {/* <Community /> */}
-
-      {/* <Newsletter /> */}
-
-      {/* <Activity /> */}
     </>
   )
 }
