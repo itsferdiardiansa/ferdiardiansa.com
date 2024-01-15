f1ceb18da3e434aa6e7022636b10e1eea7c5a042
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2024-01-16 00:55:15 +0700

    refactor: integrate @oxcyn/types

--- apps/web/landing/src/app/layout.tsx
+++ apps/web/landing/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { PropsWithChildren } from 'react'
 import { Metadata, Viewport } from 'next'
 import { unstable_cache as cache } from 'next/cache'
 import { getBaseMetadata } from '@/firestore/collections/metadata'
@@ -20,11 +21,7 @@ export async function generateMetadata(): Promise<Metadata> {
   return { ...data }
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className="dark:bg-primary bg-zinc-200">{children}</body>
--- apps/web/landing/src/firestore/collections/metadata/metadata.ts
+++ apps/web/landing/src/firestore/collections/metadata/metadata.ts
@@ -1,42 +1,19 @@
 import { DocumentData, SnapshotMetadata, doc, getDoc } from 'firebase/firestore'
+import { SuccessResponseData } from '@oxcyn/response'
 import db from '@/firestore'
 
-// enum ErrorCode {
-//   UNAUTHORIZED          = 401,
-//   FORBIDDEN             = 403,
-//   NOT_FOUND              = 404,
-//   INTERNAL_SERVER_ERROR = 500
-// }
-
-type SuccesResponseBaseMetadata = {
-  kind: `oxcyn#${string}`
-  data: DocumentData
-  metadata: SnapshotMetadata
-}
-
-// type ErrorResponse = {
-//   code: ErrorCode
-//   message: string | undefined
-//   errors: string[]
-// }
-
-export async function getBaseMetadata(): Promise<SuccesResponseBaseMetadata> {
+export async function getBaseMetadata(): Promise<
+  SuccessResponseData<DocumentData, SnapshotMetadata>
+> {
   const ref = doc(db, 'metadata', 'baseMetadata')
   const docBaseMetadataSnap = await getDoc(ref)
 
   const { data: documentData, ...restData } = docBaseMetadataSnap
 
-  // console.log("getBaseMetadat invoked")
   return {
     kind: 'oxcyn#getBaseMetadata',
+    code: 200,
     data: <DocumentData>docBaseMetadataSnap.data(),
     ...restData,
   }
-  // } catch (e) {
-  //   return {
-  //     code: ErrorCode.NOT_FOUND,
-  //     message: "Document doesn't exist!",
-  //     errors: []
-  //   }
-  // }
 }
